67a20dddeae17ec963f16c12874960ddd259641c
Hifumisama <m.khayat92@gmail.com>
2017-06-06 16:22:15 +0200

    Ajout du Formulaire dans appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sododo',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Prenez le temps de bien dormir !';
 }
